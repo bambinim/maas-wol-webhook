37afaf4c62d19b506b67c4b2868bc94495b25009
Matteo Bambini <matteo.bambini00@gmail.com>
2022-07-18 12:13:54 +0200

    Completed and tested all power functions

--- app.py
+++ app.py
@@ -1,25 +1,63 @@
-from flask import Flask, request
+#!/usr/bin/env python
+from flask import Flask, request, Response
 import settings
+from machine import Machine
 
 settings.load_machines()
 
 app = Flask(__name__)
 
 
-@app.route('/startup')
+def machine_from_token(token: str) -> Machine or None:
+    for i in settings.MACHINES:
+        if i.token() == token:
+            return i
+    return None
+
+
+def machine_from_name(name: str) -> Machine or None:
+    for i in settings.MACHINES:
+        if i.name() == name:
+            return i
+    return None
+
+
+@app.route('/startup', methods=['POST'])
 def startup():
-    print(request)
-    return ''
+    if 'Authorization' not in request.headers:
+        return Response({'status': 400, 'message': 'Token not found'}, status=400)
+    token = request.headers['Authorization'].replace('Bearer ', '')
+    machine = machine_from_token(token)
+    if machine is None:
+        return Response({'status': 400, 'message': 'Token not valid'}, status=400)
+    machine.startup()
+    return {'status': 200, 'message': 'WoL magic packed sent'}
 
 
-@app.route('/shutdown')
+@app.route('/shutdown', methods=['POST'])
 def shutdown():
-    return ''
+    try:
+        if 'Authorization' not in request.headers:
+            return Response({'status': 400, 'message': 'Token not found'}, status=400)
+        token = request.headers['Authorization'].replace('Bearer ', '')
+        machine = machine_from_token(token)
+        if machine is None:
+            return Response({'status': 400, 'message': 'Token not valid'}, status=400)
+        machine.shutdown(ssh_key_file=settings.SSH_KEY_PATH)
+        return {'status': 200, 'message': 'Shutdown command sent'}
+    except Exception:
+        pass
 
 
-@app.route('/status')
+@app.route('/status', methods=['GET'])
 def status():
-    return ''
+    if 'name' not in request.args:
+        return Response({'status': 400, 'message': 'Machine name not specified'}, status=400)
+    machine = machine_from_name(request.args['name'])
+    return {
+        'status': 200,
+        'power_status': 'running' if machine.status() else 'stopped'
+    }
 
 
 if __name__ == '__main__':
--- machine.py
+++ machine.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import wakeonlan as wol
 import subprocess
+from paramiko import SSHClient, RSAKey, AutoAddPolicy
 
 
 class Machine:
@@ -12,11 +13,22 @@ class Machine:
         self.__token = token
         self.__ssh_user = ssh_user
 
+    def name(self) -> str:
+        return self.__name
+
+    def token(self) -> str:
+        return self.__token
+
     def startup(self):
         wol.send_magic_packet(self.__mac)
 
-    def shutdown(self, key_file: str or Path = None):
-        pass
+    def shutdown(self, ssh_key_file: str or Path):
+        key = RSAKey.from_private_key_file(ssh_key_file)
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(AutoAddPolicy())
+        ssh.connect(hostname=self.__ip, username=self.__ssh_user, pkey=key)
+        ssh.exec_command('sudo shutdown now')
+        ssh.close()
 
     def status(self) -> bool:
         proc = subprocess.Popen(['/usr/bin/ping', '-c', '1', self.__ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
