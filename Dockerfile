359cbff7f5e96d153d6a434248c9384d1370d759
Matteo Bambini <matteo.bambini00@gmail.com>
2022-07-18 20:37:24 +0200

    Updated Dockerfile and added docker compose file

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM python:3
+FROM python:3.8
 
 ENV HOST 0.0.0.0
 ENV PORT 8080
-ENV SSH_KEY_PATH /ssh/id_rsa
-
-COPY ./* /srv
+ENV SSH_KEY_PATH ~/.ssh/id_rsa
 
 WORKDIR /srv
+COPY ./ ./
 RUN pip install -r requirements.txt
 
-ENTRYPOINT '/srv/app.py'
+ENTRYPOINT ["python", "app.py"]
--- settings.py
+++ settings.py
@@ -6,8 +6,6 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(os.path.dirname(__file__))
 
-CONFIG_FILE = BASE_DIR / 'config.yaml'
-
 if os.path.exists(BASE_DIR / '.env.local'):
     load_dotenv(BASE_DIR / '.env.local')
 elif os.path.exists(BASE_DIR / '.env'):
@@ -17,6 +15,7 @@ elif os.path.exists(BASE_DIR / '.env'):
 HOST = os.environ.get('HOST') or '127.0.0.1'
 PORT = os.environ.get('PORT') or 8000
 SSH_KEY_PATH = os.environ.get('SSH_KEY_PATH') or Path('~/.ssh/id_rsa')
+CONFIG_FILE = os.environ.get('CONFIG_FILE') or BASE_DIR / 'config.yaml'
 
 
 MACHINES = []
